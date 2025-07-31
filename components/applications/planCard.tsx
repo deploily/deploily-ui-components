4e4a0b1b327a6487ac6e337dce292a48070b8945
ziadi kheira <kheira.ziadi@transformatek.dz>
2025-07-31 16:02:30 +0000

    Enhance PlanCard component with button for custom plans

--- components/applications/planCard.tsx
+++ components/applications/planCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Check } from '@phosphor-icons/react';
-import { Card, Col, Row, theme, Typography } from 'antd';
+import { Button, Card, Col, Row, theme, Typography } from 'antd';
 
 
 export interface Props {
@@ -13,8 +13,12 @@ export interface Props {
         icon: string;
         html_content: string;
     }[];
-    onClick: any,
-    styles: any
+    onClick: any;
+    showModal: any;
+    styles: any;
+    isCustomPlan: boolean;
+    customPlanSelected?: boolean;
+    translations: any;
 }
 
 export default function PlanCard({
@@ -22,12 +26,19 @@ export default function PlanCard({
     styles,
     title,
     price,
-    subscription_category,
     options,
-    onClick
+    onClick,
+    isCustomPlan,
+    customPlanSelected,
+    translations,
+    showModal,
 }: Props) {
+    console.log('IS CUSTOM PLAN', isCustomPlan
+        , "translations", translations
+    );
 
-    const {token}=theme.useToken()
+
+    const { token } = theme.useToken()
 
     return (
         <Card
@@ -38,55 +49,78 @@ export default function PlanCard({
                 height: "100%",
                 width: "100%",
                 borderColor: styles.color ?? 'grey',
-                boxShadow:styles.boxShadow,
-                minWidth: 200,
-                maxWidth: 250,
+                boxShadow: styles.boxShadow,
+                minWidth: 220,
+                maxWidth: 300,
             }}
             styles={{
                 body: { flex: 1, display: "flex", flexDirection: "column", paddingBottom: 0 },
             }}
         >
-  
-                <Typography.Title level={3} style={{ textAlign: "center"}}>
-                    {title}
-                </Typography.Title>
 
+            <Typography.Title level={3} style={{ textAlign: "center" }}>
+                {title}
+            </Typography.Title>
+
+            {isCustomPlan ?
                 <Typography.Paragraph
-                    style={{ fontSize: 25, fontWeight: 600, textAlign: "center",color: token.colorPrimary }}
+                    style={{ fontSize: 20, fontWeight: 600, textAlign: "center", color: token.colorPrimary }}
+                >
+                    {translations["onDemand"]}
+                </Typography.Paragraph> :
+                <Typography.Paragraph
+                    style={{ fontSize: 25, fontWeight: 600, textAlign: "center", color: token.colorPrimary }}
                 >
                     {Intl.NumberFormat("fr-FR", { useGrouping: true }).format(price)}
                     <span style={{ fontSize: 16, fontWeight: 400 }}>
                         {" "}
-                        DZD/
-                        {subscription_category}
+                        {translations[`DZD`]}
+                        {" / "}
+                        {translations[`subscription_category`]}
                     </span>
                 </Typography.Paragraph>
+            }
 
-                <div style={{ flex: 1, paddingBottom: "16px" }}>
-                    {options.map((row) => (
-                        <Row gutter={16} key={row.id} align="middle">
-                            <Col span={3}>
-                                {row.icon ? row.icon : <Check size={24}  />}
-                            </Col>
-                            <Col span={21}>
-                                <Typography.Paragraph
-                                    style={{
-                                        fontSize: 16,
-                                        margin: 0,
-                                        display: "flex",
-                                        alignItems: "center",
-                                        minHeight: 24,
-                                    }}
-                                >
-                                    <div
-                                        dangerouslySetInnerHTML={{ __html: row.html_content }}
-                                        style={{ margin: 0, lineHeight: "24px" }}
-                                    />
-                                </Typography.Paragraph>
-                            </Col>
-                        </Row>
-                    ))}
-                </div>
+            <div style={{ flex: 1, paddingBottom: "16px" }}>
+                {options.map((row) => (
+                    <Row gutter={16} key={row.id} align="middle">
+                        <Col span={3}>
+                            {row.icon ? row.icon : <Check size={24} />}
+                        </Col>
+                        <Col span={21}>
+                            <Typography.Paragraph
+                                style={{
+                                    fontSize: 16,
+                                    margin: 0,
+                                    display: "flex",
+                                    alignItems: "center",
+                                    minHeight: 24,
+                                }}
+                            >
+                                <div
+                                    dangerouslySetInnerHTML={{ __html: row.html_content }}
+                                    style={{ margin: 0, lineHeight: "24px" }}
+                                />
+                            </Typography.Paragraph>
+                        </Col>
+                    </Row>
+                ))}
+            </div>
+
+            {isCustomPlan && customPlanSelected && <Button
+                onClick={showModal}
+                style={{
+                    backgroundColor: token.colorPrimary,
+                    color: token.colorTextLightSolid,
+                    border: "none",
+                    width: "100%",
+                    marginBottom: 20,
+                    fontWeight: 600,
+                    fontSize: 20,
+                }}
+            >
+                {translations["contactUs"]}
+            </Button>}
         </Card>
-        );
+    );
 }
